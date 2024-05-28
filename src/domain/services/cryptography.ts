1d8cb5dd8ac6b45cf2beeab88ed3a76dc6817823
jeseias <domingosjeseias@gmail.com>
2024-05-28 10:41:09 +0100

    feat: add Cryptography gateway service

---
+++ src/domain/services/cryptography.ts
@@ -0,0 +1,25 @@
+export namespace Cryptography {
+  export namespace Hasher {
+    export interface Contract {
+      hash(value: string): Promise<string>;
+    }
+  }
+
+  export namespace HashCompare {
+    export interface Contract {
+      compare(params: { value: string; hash: string }): Promise<boolean>;
+    }
+  }
+
+  export namespace Encrypter {
+    export interface Contract {
+      encrypt(value: any, expiresIn: string | number): Promise<string>;
+    }
+  }
+
+  export namespace Decrypter {
+    export interface Contract<R> {
+      decrypt(value: any): Promise<R>;
+    }
+  }
+}
