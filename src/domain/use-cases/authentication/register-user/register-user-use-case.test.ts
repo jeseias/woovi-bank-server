9a4a57493427d3cd071195967246be7f4a606819
jeseias <domingosjeseias@gmail.com>
2024-05-28 11:31:24 +0100

    test: refactor

--- src/domain/use-cases/authentication/register-user/register-user-use-case.test.ts
+++ src/domain/use-cases/authentication/register-user/register-user-use-case.test.ts
@@ -34,58 +34,64 @@ const makeSut = () => {
 
 describe("RegisterUserUseCase", () => {
   describe("Execute()", () => {
-    it("Should call findUser() with the correct params", async () => {
-      const { sut, userRepository } = makeSut();
-      const params = mockParams();
+    describe("UserRepository", () => {
+      it("Should call findUser() with the correct params", async () => {
+        const { sut, userRepository } = makeSut();
+        const params = mockParams();
 
-      await sut.execute(params);
+        await sut.execute(params);
 
-      expect(userRepository.findUser).toHaveBeenCalledTimes(1);
-      expect(userRepository.findUser).toHaveBeenCalledWith({
-        tax_id: params.tax_id,
+        expect(userRepository.findUser).toHaveBeenCalledTimes(1);
+        expect(userRepository.findUser).toHaveBeenCalledWith({
+          tax_id: params.tax_id,
+        });
+      });
+
+      it('Should return Error("User already exists") if a user if found', async () => {
+        const { sut } = makeSut();
+
+        const result = await sut.execute(mockParams());
+
+        expect(result).toEqual(new Error("User already exists"));
+      });
+
+      it("Should call createUser() 1 time and with the correct params", async () => {
+        const { sut, userRepository } = makeSut();
+        (userRepository.findUser as Mock).mockResolvedValue(null);
+
+        await sut.execute(mockParams());
+
+        expect(userRepository.createUser).toHaveBeenCalledTimes(1);
+        expect(userRepository.createUser).toHaveBeenCalledWith(mockParams());
       });
     });
 
-    it('Should return Error("User already exists") if a user if found', async () => {
-      const { sut } = makeSut();
+    describe("AccountRepository", () => {
+      it("Should call createAccount() 1 time and with the correct params", async () => {
+        const { sut, userRepository, accountRepository } = makeSut();
+        (userRepository.findUser as Mock).mockResolvedValue(null);
 
-      const result = await sut.execute(mockParams());
+        await sut.execute(mockParams());
 
-      expect(result).toEqual(new Error("User already exists"));
-    });
-
-    it("Should call createUser() 1 time and with the correct params", async () => {
-      const { sut, userRepository } = makeSut();
-      (userRepository.findUser as Mock).mockResolvedValue(null);
-
-      await sut.execute(mockParams());
-
-      expect(userRepository.createUser).toHaveBeenCalledTimes(1);
-      expect(userRepository.createUser).toHaveBeenCalledWith(mockParams());
-    });
-
-    it("Should call createAccount() 1 time and with the correct params", async () => {
-      const { sut, userRepository, accountRepository } = makeSut();
-      (userRepository.findUser as Mock).mockResolvedValue(null);
-
-      await sut.execute(mockParams());
-
-      expect(accountRepository.createAccount).toHaveBeenCalledTimes(1);
-      expect(accountRepository.createAccount).toHaveBeenCalledWith({
-        user_id: "any_id",
-        account_number: expect.any(String),
+        expect(accountRepository.createAccount).toHaveBeenCalledTimes(1);
+        expect(accountRepository.createAccount).toHaveBeenCalledWith({
+          user_id: "any_id",
+          account_number: expect.any(String),
+        });
       });
     });
 
-    it("Should call hash() 1 time and with the correct params", async () => {
-      const { sut, userRepository, accountRepository, hashRepository } =
-        makeSut();
-      (userRepository.findUser as Mock).mockResolvedValue(null);
+    describe("Cryptography", () => {
+      it("Should call hash() 1 time and with the correct params", async () => {
+        const { sut, userRepository, accountRepository, hashRepository } =
+          makeSut();
+        (userRepository.findUser as Mock).mockResolvedValue(null);
 
-      await sut.execute(mockParams());
+        await sut.execute(mockParams());
 
-      expect(hashRepository.hash).toHaveBeenCalledTimes(1);
-      expect(hashRepository.hash).toHaveBeenCalledWith("any_id");
+        expect(hashRepository.hash).toHaveBeenCalledTimes(1);
+        expect(hashRepository.hash).toHaveBeenCalledWith("any_id");
+      });
     });
 
     it("Should return user data and token on success", async () => {
