cf0dfb72d4eff7f516c6222d14e834480d8d151c
jeseias <domingosjeseias@gmail.com>
2024-05-30 14:38:43 +0100

    feat: add update and findAccount to AccountMongooseRepository

--- src/infra/db/repositories/account-mongoose-repository.ts
+++ src/infra/db/repositories/account-mongoose-repository.ts
@@ -2,6 +2,8 @@ import { AccountRepository } from "@/domain/repositories/accounts";
 import { CreateAccountRepository } from "@/domain/repositories/accounts/create-account-repository";
 import { AccountDocument, AccountModel } from "../models/account-model";
 import { Account } from "@/domain/entities";
+import { FindAccountRepository } from "@/domain/repositories/accounts/find-account-repository";
+import { UpdateAccountRepository } from "@/domain/repositories/accounts/update-account-repository";
 
 export class AccountMongooseRepository implements AccountRepository {
   async createAccount(
@@ -9,6 +11,23 @@ export class AccountMongooseRepository implements AccountRepository {
   ): CreateAccountRepository.Response {
     const account = await AccountModel.create(params);
     return this.toDomain(account);
+  }
+
+  async findAccount(
+    params: FindAccountRepository.Params
+  ): FindAccountRepository.Response {
+    const account = await AccountModel.findOne(params).lean();
+    return account && this.toDomain(account);
+  }
+
+  async update(
+    params: UpdateAccountRepository.Params
+  ): UpdateAccountRepository.Response {
+    const account = await AccountModel.findByIdAndUpdate(params.user_id, {
+      balance: params.balance,
+    }).lean();
+
+    return account! && this.toDomain(account);
   }
 
   private toDomain(account: AccountDocument) {
