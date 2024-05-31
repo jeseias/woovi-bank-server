572748a4d9ec85f0ef35d1392e64d5ab2e3bd0ac
jeseias <domingosjeseias@gmail.com>
2024-05-31 11:39:14 +0100

    feat: add findAllByUser TransactionMongooseRepository

--- src/domain/repositories/transactions/index.ts
+++ src/domain/repositories/transactions/index.ts
@@ -1,5 +1,7 @@
 import { CreateTransactionRepository } from "./create-transaction-repository";
 import { FindTransactionRepository } from "./find-transaction-repository";
+import { FindTransactionsByUserRepository } from "./find-transactions-by-user-repository";
 
 export type TransactionRepository = CreateTransactionRepository.Contract &
-  FindTransactionRepository.Contract;
+  FindTransactionRepository.Contract &
+  FindTransactionsByUserRepository.Contract;
--- src/infra/db/repositories/transaction-mongoose-repository.ts
+++ src/infra/db/repositories/transaction-mongoose-repository.ts
@@ -6,6 +6,7 @@ import {
 } from "../models/transaction-model";
 import { Transaction } from "@/domain/entities";
 import { FindTransactionRepository } from "@/domain/repositories/transactions/find-transaction-repository";
+import { FindTransactionsByUserRepository } from "@/domain/repositories/transactions/find-transactions-by-user-repository";
 
 export class TransactionMongooseRepository implements TransactionRepository {
   async create(
@@ -22,6 +23,23 @@ export class TransactionMongooseRepository implements TransactionRepository {
   ): FindTransactionRepository.Response {
     const transaction = await TransactionModel.findOne(params);
     return transaction && this.toDomain(transaction);
+  }
+
+  async findAllByUser(
+    params: FindTransactionsByUserRepository.Params
+  ): FindTransactionsByUserRepository.Response {
+    const transactions = await TransactionModel.find({
+      $or: [
+        {
+          [Transaction.Fields.Receiver_Id]: params.user_id,
+        },
+        {
+          [Transaction.Fields.Sender_Id]: params.user_id,
+        },
+      ],
+    }).lean();
+
+    return transactions.map((item) => this.toDomain(item));
   }
 
   private toDomain(transaction: TransactionDocument) {
