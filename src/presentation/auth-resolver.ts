726041396532f79b222c5e3599f2f48dcdf65c2a
jeseias <domingosjeseias@gmail.com>
2024-05-31 11:00:05 +0100

    feat: add login resolver

--- src/presentation/auth-resolver.ts
+++ src/presentation/auth-resolver.ts
@@ -1,16 +1,25 @@
 import { CreateUserRepository } from "@/domain/repositories/users/create-user-repository";
-import { makeRegisterUser } from "@/main/factories/use-case-factories";
+import { ILoginUserParams } from "@/domain/use-cases/authentication/login-user";
+import {
+  makeLogin,
+  makeRegisterUser,
+} from "@/main/factories/use-case-factories";
 
 const registerUser = makeRegisterUser();
+const loginUser = makeLogin();
 
 export const authResolvers = {
   Mutation: {
     registerUser: async (_, arg: CreateUserRepository.Params) => {
-      const user = await registerUser.execute({
+      const result = await registerUser.execute({
         ...arg,
       });
 
-      return user;
+      return result;
+    },
+    login: async (_, args: ILoginUserParams) => {
+      const result = await loginUser.execute(args);
+      return result;
     },
   },
 };
