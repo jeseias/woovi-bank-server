22a5de276507b895464e9d44d871d7b7f0d4da15
jeseias <domingosjeseias@gmail.com>
2024-05-31 10:49:07 +0100

    feat: remove logic to generate account number from the RegisterUserUseCAse

--- src/domain/use-cases/authentication/register-user/register-user-use-case.ts
+++ src/domain/use-cases/authentication/register-user/register-user-use-case.ts
@@ -3,6 +3,7 @@ import type { UserRepository } from "domain/repositories/users";
 import type { CreateUserRepository } from "domain/repositories/users/create-user-repository";
 import type { CryptoRepository } from "domain/services";
 import { AuthHelpers } from "../auth-helpers/auth-helpers";
+import { Account } from "@/domain/entities";
 
 export class RegisterUserUseCase extends AuthHelpers {
   constructor(
@@ -13,53 +14,41 @@ export class RegisterUserUseCase extends AuthHelpers {
     super();
   }
 
-  generateAccountNumber(): string {
-    const chars =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let randomString = "";
-    for (let i = 0; i < 6; i++) {
-      randomString += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-
-    const now = new Date();
-    const day = String(now.getDate()).padStart(2, "0");
-    const month = String(now.getMonth() + 1).padStart(2, "0");
-    const year = now.getFullYear();
-
-    const formattedDate = `${day}/${month}/${year}`;
-
-    return `${randomString}${formattedDate}`;
-  }
-
   async execute(params: CreateUserRepository.Params) {
-    const userAlreadyExists = await this.userRepository.findUser({
-      tax_id: params["tax_id"],
-    });
+    try {
+      const userAlreadyExists = await this.userRepository.findUser({
+        tax_id: params["tax_id"],
+      });
 
-    if (userAlreadyExists) {
-      return new Error("User already exists");
+      if (userAlreadyExists) {
+        return new Error("User already exists");
+      }
+
+      const hashedPassword = await this.cryptoRepository.hash(
+        params["password"]
+      );
+      const user = await this.userRepository.createUser({
+        ...params,
+        password: hashedPassword,
+      });
+
+      const account_number = Account.generateAccountNumber();
+      await this.accountRepository.createAccount({
+        user_id: user["id"],
+        account_number,
+      });
+
+      const token = await this.cryptoRepository.encrypt(
+        { id: user["id"] },
+        this.generationTokenExpirationDate()
+      );
+
+      return {
+        user,
+        token,
+      };
+    } catch (error) {
+      console.log(error);
     }
-
-    const hashedPassword = await this.cryptoRepository.hash(params["password"]);
-    const user = await this.userRepository.createUser({
-      ...params,
-      password: hashedPassword,
-    });
-
-    const account_number = this.generateAccountNumber();
-    await this.accountRepository.createAccount({
-      user_id: user["id"],
-      account_number,
-    });
-
-    const token = await this.cryptoRepository.encrypt(
-      { id: user["id"] },
-      this.generationTokenExpirationDate()
-    );
-
-    return {
-      user,
-      token,
-    };
   }
 }
