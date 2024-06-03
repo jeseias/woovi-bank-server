71287aaa406b6f32057d92c1571f241c21d811ab
jeseias <domingosjeseias@gmail.com>
2024-06-03 10:40:22 +0100

    feat: add validation using graphql inputs

--- src/presentation/resolvers/auth-resolver.ts
+++ src/presentation/resolvers/auth-resolver.ts
@@ -1,5 +1,5 @@
 import { CreateUserRepository } from "@/domain/repositories/users/create-user-repository";
-import { ILoginUserParams } from "@/domain/use-cases/authentication/login-user";
+import { ILoginUserParams } from "@/domain/use-cases/authentication";
 import {
   makeLogin,
   makeRegisterUser,
@@ -10,15 +10,15 @@ const loginUser = makeLogin();
 
 export const authResolvers = {
   Mutation: {
-    registerUser: async (_: any, arg: CreateUserRepository.Params) => {
-      const result = await registerUser.execute({
-        ...arg,
-      });
-
+    registerUser: async (
+      _: any,
+      args: { input: CreateUserRepository.Params }
+    ) => {
+      const result = await registerUser.execute(args.input);
       return result;
     },
-    login: async (_: any, args: ILoginUserParams) => {
-      const result = await loginUser.execute(args);
+    login: async (_: any, args: { input: ILoginUserParams }) => {
+      const result = await loginUser.execute(args.input);
       return result;
     },
   },
