7264d58b24665a3296c80b65c183e9c39c9f8874
jeseias <domingosjeseias@gmail.com>
2024-06-23 14:31:01 +0100

    test: fux calculate account available balance test

--- src/domain/use-cases/accounts/calculate-account-available-balance/calculate-account-available-balance.spec.ts
+++ src/domain/use-cases/accounts/calculate-account-available-balance/calculate-account-available-balance.spec.ts
@@ -1,20 +1,41 @@
 import {
+  mockAccountModel,
+  mockAccountRepository,
+} from "../../__mocks__/account.mocks";
+import {
   mockTransactionModel,
   mockTransactionRepository,
 } from "../../__mocks__/transaction.mocks";
+import { mockUserRepository } from "../../__mocks__/user.mocks";
 import { CalculateAccountAvailableBalance } from "./calculate-account-available-balance-use-case";
 
 const makeSut = () => {
   const transactionRepository = mockTransactionRepository();
-  const sut = new CalculateAccountAvailableBalance(transactionRepository);
+  const accountRepository = mockAccountRepository();
+  const sut = new CalculateAccountAvailableBalance(
+    transactionRepository,
+    accountRepository
+  );
 
   return {
     sut,
     transactionRepository,
+    accountRepository,
   };
 };
 
 describe("CalculateAccountAvailableBalance", () => {
+  it("Should call findAccount with the correct values", async () => {
+    const { sut, accountRepository } = makeSut();
+
+    await sut.execute({ user_id: "any_id" });
+
+    expect(accountRepository.findAccount).toHaveBeenCalledTimes(1);
+    expect(accountRepository.findAccount).toHaveBeenCalledWith({
+      user_id: "any_id",
+    });
+  });
+
   it("Should call findAllByUser() with the correct values", async () => {
     const { sut, transactionRepository } = makeSut();
 
@@ -27,7 +48,12 @@ describe("CalculateAccountAvailableBalance", () => {
   });
 
   it("Should return balance of 0 if the user has 0 transactions", async () => {
-    const { sut, transactionRepository } = makeSut();
+    const { sut, transactionRepository, accountRepository } = makeSut();
+    jest.spyOn(accountRepository, "findAccount").mockResolvedValue(
+      mockAccountModel({
+        balance: 0,
+      })
+    );
     transactionRepository.findAllByUser.mockResolvedValue([]);
 
     const result = await sut.execute({ user_id: "any_id" });
@@ -36,7 +62,12 @@ describe("CalculateAccountAvailableBalance", () => {
   });
 
   it("Should return balance of 750", async () => {
-    const { sut, transactionRepository } = makeSut();
+    const { sut, transactionRepository, accountRepository } = makeSut();
+    jest.spyOn(accountRepository, "findAccount").mockResolvedValue(
+      mockAccountModel({
+        balance: 0,
+      })
+    );
     transactionRepository.findAllByUser.mockResolvedValue([
       mockTransactionModel({
         receiver: "any_id",
