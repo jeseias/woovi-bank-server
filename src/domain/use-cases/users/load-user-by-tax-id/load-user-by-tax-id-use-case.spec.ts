1d31fd0977cb66462d5059800b5dcfae8f9e8bc4
jeseias <domingosjeseias@gmail.com>
2024-06-23 14:21:58 +0100

    test: LoadUserByTaxIdUseCase

---
+++ src/domain/use-cases/users/load-user-by-tax-id/load-user-by-tax-id-use-case.spec.ts
@@ -0,0 +1,42 @@
+import { mockUserModel, mockUserRepository } from "../../__mocks__/user.mocks";
+import { LoadUserByTaxIdUseCase } from "./load-user-by-tax-id-use-case";
+
+const makeSut = () => {
+  const userRepository = mockUserRepository();
+  const sut = new LoadUserByTaxIdUseCase(userRepository);
+
+  return {
+    sut,
+    userRepository,
+  };
+};
+
+describe("LoadUserByTaxIdUseCase", () => {
+  it("Should call userRepository.findUser() with the correct values", async () => {
+    const { sut, userRepository } = makeSut();
+
+    await sut.execute({ tax_id: "any_tax_id" });
+
+    expect(userRepository.findUser).toHaveBeenCalledTimes(1);
+    expect(userRepository.findUser).toHaveBeenCalledWith({
+      tax_id: "any_tax_id",
+    });
+  });
+
+  it("Should return user on success", async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute({ tax_id: "any_tax_id" });
+
+    expect(result).toEqual({ user: mockUserModel() });
+  });
+
+  it('Should return Error("Not found") if user is not found', async () => {
+    const { sut, userRepository } = makeSut();
+    jest.spyOn(userRepository, "findUser").mockResolvedValue(null);
+
+    const result = await sut.execute({ tax_id: "any_id" });
+
+    expect(result).toEqual(new Error("User not found"));
+  });
+});
---
+++ src/domain/use-cases/users/load-user-by-tax-id/load-user-by-tax-id-use-case.ts
@@ -0,0 +1,15 @@
+import { UserRepository } from "@/domain/repositories/users";
+
+export class LoadUserByTaxIdUseCase {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async execute({ tax_id }: { tax_id: string }) {
+    const user = await this.userRepository.findUser({ tax_id });
+
+    if (!user) {
+      return new Error("User not found");
+    }
+
+    return { user };
+  }
+}
